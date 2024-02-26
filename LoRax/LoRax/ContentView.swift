9f3d5b8a6985af16b65de74b887ec98d4bd206a6
maxjtodd <maxtodd@mail.com>
2024-02-26 15:23:51 -0700

    first commit

---
+++ LoRax/LoRax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoRax
+//
+//  Created by Max Todd on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoRax/LoRax/LoRaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoRaxApp.swift
+//  LoRax
+//
+//  Created by Max Todd on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LoRaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
