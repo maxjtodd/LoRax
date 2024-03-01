58ba3b38844836f938ec40059a2b975330ddf030
Ben Weber <bjw32494@gmail.com>
2024-02-29 23:53:13 -0700

    basic ui elemements and rm DS_stores

--- LoRax/LoRax/ContentView.swift
+++ LoRax/LoRax/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            TextField("Enter your message", text: $message)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button(action: {
+                    print("Chat button pushed")
+                }) {
+                    Text("Chat")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    print("Advanced button pushed")
+                }) {
+                    Text("Advanced")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                }
+            }
             .padding()
+        }
+        .padding()
     }
 }
 
