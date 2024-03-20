35e6a069b9d709921beac32127c6d28df14ae848
brockmiddleton <brockmiddleton4@gmail.com>
2024-03-20 13:46:19 -0600

    Update gitignore

---
+++ hardware/multithread_sketch/multithread/multithread.ino
@@ -0,0 +1,108 @@
+TaskHandle_t Task0;
+TaskHandle_t Task1;
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include "heltec.h" // Include Heltec OLED library
+
+#define SERVICE_UUID        "12345678-1234-1234-1234-123456789abc"
+#define CHARACTERISTIC_UUID "abcdefab-cdef-1234-5678-123456789abc"
+
+// Bluetooth class
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) {
+      Serial.println("Device connected");
+      Heltec.display->clear();
+      // try to to retrieve the name of the connected device
+      std::string name = "Device";
+      if (param->connect.remote_bda) {
+        char addr_str[30];
+        sprintf(addr_str, "%02x:%02x:%02x:%02x:%02x:%02x",
+                param->connect.remote_bda[0], param->connect.remote_bda[1], param->connect.remote_bda[2],
+                param->connect.remote_bda[3], param->connect.remote_bda[4], param->connect.remote_bda[5]);
+        name += " (";
+        name += addr_str;
+        name += ")";
+      }
+      Heltec.display->drawString(0, 0, "Connected to:");
+      Heltec.display->drawString(0, 10, name.c_str());
+      Heltec.display->display();
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+      Serial.println("Device disconnected");
+      Heltec.display->clear();
+      Heltec.display->drawString(0, 0, "Device disconnected");
+      Heltec.display->display();
+      // restart advertising after disconnection
+      pServer->startAdvertising();
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  //create a task that executes the Bluetoothcode() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(Bluetoothcode, "Task0", 10000, NULL, 1, &Task0, 0);
+  //create a task that executes the SendReceive() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(SendReceivecode, "Task1", 10000, NULL, 1, &Task1, 1);
+}
+
+void loop() {
+  // nothing to do here, everything happens in the Task1Code and Task2Code functions
+  Serial.print("main loop on core ");
+  Serial.println(xPortGetCoreID());
+  delay(3000);
+}
+
+
+void Bluetoothcode(void* pvParameters) {
+  Serial.print("Task0 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  // initialize OLED display
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, "BLE Server Start");
+  Heltec.display->display();
+
+  // initialize BLE
+  BLEDevice::init("Heltec BLE Server");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+  pCharacteristic->setValue("Hello BLE");
+  pService->start();
+  pServer->getAdvertising()->start();
+  
+  Serial.println("BLE server is ready, waiting for connections...");
+
+  for (;;) { // infinite loop
+    Serial.println("  Core 0 processing - bluetooth");
+    delay(1000);
+
+  }
+}
+
+void SendReceivecode(void* pvParameters) {
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;) {
+    Serial.println("  Core 1 processing - send/receive");
+    delay(1000);
+
+    /* INSERT CODE TO MONITOR RECEIVING & SENDING */
+  }
+}
+
+
+
