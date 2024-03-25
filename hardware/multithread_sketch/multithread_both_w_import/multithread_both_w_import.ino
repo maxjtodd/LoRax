6f4d06ce659cefb6c6b59467b6e225a4897f2308
brockmiddleton <brockmiddleton4@gmail.com>
2024-03-25 13:57:46 -0600

    Add code to multithreading - both LoRa and BT
    This is a working sketch of Bluetooth and LoRa Send/Receive with each running on an individual thread. Code for each are separated into their own cpp files and imported via header.

---
+++ hardware/multithread_sketch/multithread_both_w_import/multithread_both_w_import.ino
@@ -0,0 +1,35 @@
+
+
+#include "LoRaWan_APP.h"
+#include <Wire.h>             //| Included via heltec.h
+//#include "HT_SSD1306Wire.h" //|  |
+#include "Arduino.h"          //| Included via loranwan
+
+#include "BluetoothHandler.h"
+//#include "C:\Users\brock\OneDrive\Desktop\CS448\LoRax_repo\hardware\multithread_sketch\multithread_btex\BluetoothHandler.cpp"
+
+#include "LoRa.h"
+
+
+TaskHandle_t Task0;
+TaskHandle_t Task1;
+
+void setup() {
+
+
+  Serial.begin(115200);
+
+  //create a task that executes the Bluetoothcode() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(Bluetoothcode, "Task0", 10000, NULL, 1, &Task0, 0);
+  //create a task that executes the SendReceive() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(SendReceivecode, "Task1", 10000, NULL, 1, &Task1, 1);
+}
+
+void loop() {
+  // nothing to do here, everything happens in the Task1Code and Task2Code functions
+  Serial.print("main loop on core ");
+  Serial.println(xPortGetCoreID());
+  delay(3000);
+}
+
+
