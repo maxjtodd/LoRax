38f1b828ca63735fb5db0a053170409fcf284219
brockmiddleton <brockmiddleton4@gmail.com>
2024-04-22 12:36:45 -0600

    Add string processing, refine message sending, contact ping
    String processing to intake and build messageData from received string.
    Fix error from null ptr after sending message - default to Receive after sending

--- hardware/multithread_sketch/multithread_both_w_import/LoRa.cpp
+++ hardware/multithread_sketch/multithread_both_w_import/LoRa.cpp
@@ -2,6 +2,8 @@
 #include <Wire.h>   
 #include <ctime>
 #include <chrono>
+#include <string>
+#include <iostream>
 #include "HT_SSD1306Wire.h"
 #include "Arduino.h"
 #include "LoRa.h"
@@ -36,17 +38,68 @@ enum State
 State state;
 int16_t txNumber=0;
 int16_t pingCount = 0;
+int16_t sendMessageCount = 0;
 int16_t rssi, rxSize;
 bool lora_idle=true;
 
 // Need a third type of message - ACK's
-// everytime we send a message out, we dont pop from the queue until we receive an ACK that corresponds to the message
-int ack_list[10];
+// everytime we send a message out, we pop the message from original queue, add to ack_wait_list
+// we only need unique id of message, list of char arrays
+char* sentMessageIDs[100];
 
+const int ident_message = 0x01;
+const int ident_contact = 0x02;
+const int ident_ack = 0x03;
 
 
 // global instance of messageData - contactPing
 messageData contactPing = {ESP.getEfuseMac(), NULL, NULL};
+
+
+messageData processMessageString(char* message) {
+  /*
+  Params : (char*) message
+  
+    Process a message (char*)
+    Return a new messageData object.
+    Attributes assigned to those passed in message
+
+    MESSAGE FORMAT:
+
+      TYPE;ID:CONTENT;
+  
+  */
+
+  int type_token;
+  char* id_token;
+  char* message_token;
+  char* dest_token;
+
+  std::string delimiter = ";";
+  std::string s = message;
+
+  size_t pos = 0;
+  int count = 0;
+  std::string token;
+  // iterate thru string, separate by ';', assign each token
+  while ((pos = s.find(delimiter)) != std::string::npos) {
+      token = s.substr(0, pos);
+      char* token_char = new char[token.length()+1];
+      strcpy(token_char, token.c_str());
+      //Serial.printf("   TOKEN: %s\n", token_char);
+      s.erase(0, pos + delimiter.length());
+      
+      if (count == 0) { type_token = strtol(token_char, &id_token, 10); }
+      else if (count == 1) { id_token = token_char; }
+      else if (count == 2) { dest_token = token_char; }
+      else { message_token = token_char; }
+      count++;
+  }
+
+  messageData newMessage = {type_token, id_token, dest_token, 0, message_token };
+
+  return newMessage;
+} // processMessageString
 
 void SendReceivecode(void* vpParameters) {
 
@@ -94,33 +147,46 @@ void SendReceivecode(void* vpParameters) {
       {
         Serial.println("Building Contact Ping");
 
-        // get current time
-        // auto now = std::chrono::system_clock::now();
-        // std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-
-        // append time to conactPing obj
-        // contactPing.messageSentTime = now_c;
-
-        // broadcast ping
+        // ping printout
         pingCount++;
-        Serial.printf("Sending Ping: %d, %d\n", contactPing.id, pingCount);
-        sprintf(txpacket, "0;%d", contactPing.id);
+        Serial.printf("Sending Ping: %d, %d\n", contactPing.message_id, pingCount);
+        sprintf(txpacket, "0;%d;0;0;0", contactPing.message_id);
 
+        // send contact ping
         Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
         Radio.IrqProcess( );
-        delay(3000); // delay 2s
       
+        delay(3000); // delay 3s 
+
         break;
       }
       // CASE - message to send from iOS  
       case STATE_TX:
       { // make explicit
-        txNumber++; // increment send count
-        messageData data_to_send = getMessageData(messageDataQueue_toLora); // get data_to_send to send
-        sprintf(txpacket, "LoRa message : id %d , size : %d, value : %s", data_to_send.id, data_to_send.size, data_to_send.value);
+
+        // increment send count
+        sendMessageCount++;
+
+        // pop message packet from waiting queue
+        messageData data_to_send = getMessageData(messageDataQueue_toLora);
+
+
+        // packet prinout
+        Serial.printf("PACKET FROM BT : %d;%s;%s;%d;%s\n", data_to_send.message_type, data_to_send.message_id, data_to_send.message_dest,  data_to_send.size, data_to_send.value);
+        sprintf(txpacket, "%d;%s;%s;%d;%s", data_to_send.message_type, data_to_send.message_id, data_to_send.message_dest,  data_to_send.size, data_to_send.value);
         Serial.printf(txpacket);
-        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); //send the package out 
+
+        Serial.println("\nSending...");
+        // send the packet out
+        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); 
         Radio.IrqProcess( );
+        Serial.println("Sent.");
+
+        // // add packet ID to ack llst
+        // size_t size = sizeof(sentMessageIDs);
+        // sentMessageIDs[size] = data_to_send.message_id;
+        state = STATE_RX;
+
         break;
       }
 
@@ -146,45 +212,86 @@ void SendReceivecode(void* vpParameters) {
 
 }
 
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
-{   
-    char result[100];                 // array to store resulting string 
-    rssi=rssi;
-    rxSize=size;
+
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr ) {   
     memcpy(rxpacket, payload, size ); // copy message 
     rxpacket[size]='\0';              // terminating char
-    Radio.Sleep( );                 
-    sprintf(result,"\r\nreceived packet \"%s\" with rssi %d , length %d\r\n",rxpacket,rssi,rxSize);
+    Radio.Sleep( );                   
+
+    // build serial monitor printout
+    char result[100];                             
+    sprintf(result,"\r\nreceived packet \"%s\", length %d\r\n",rxpacket,rxSize);
     Serial.printf(result);
 
     // THREE OPTIONS ON MESSAGE RECEIVE 
         //  - Contact Ping - append to BT list w contantPing Designation
         //  - Message      - append to BT list w Message Designation
         //  - Ack of previous message send - pop associated message from LoRa queue
+    
+    /* PCKAET STRUCTURE
+
+          delim = ;
+
+      TYPE;MESSAGENUM_MACADDR;DEST_MACADDR;message_contant;
+
+    */
+
+    messageData newMessage = processMessageString(result);
+    Serial.printf(" New Message from LoRa:\n    Type:%d\n   ID:%d\n   Content:%d", newMessage.message_type, newMessage.message_id, newMessage.value);
+
 
     // Determine Packet type
-    if (rxpacket[0] == 0) {
-      Serial.println("Contact Ping Received");
+
+    // contact ping
+    if (newMessage.message_type == 0) {
+
+      // Send contact device MAC ADDR to iOS app, determine if seen before
+      Serial.println("Type is Contant Ping - push to BT Queue");
+
+      pushMessageData(messageDataQueue_toBT, newMessage);
+
       
-      // send ack 
-    }
-    else if (rxpacket[0] == 1) {
-      Serial.println("Message Received - %s", payload);
-      // create new messageData object, append to list for BT
-      messageData data_to_send;
-      data_to_send.id = rssi;
-      data_to_send.size = size;
-      data_to_send.value = rxpacket;
 
     }
 
+    // new message
+    else if (newMessage.message_type == 1) {
+
+      Serial.println("Type is Message - push to BT Queue");
+      
+      pushMessageData(messageDataQueue_toBT, newMessage);
+
+      // build, send ack
+      messageData ack = {2, newMessage.message_id, 0, 0, 0};
+      pushMessageData(messageDataQueue_toLora, ack);
+
+    }
+
+    // ack from previous sent message
     else {
-      // ack from previous message received
+      Serial.println("Type is ACK");
+
+      // // check ack list n
+      // int size = sizeof(sentMessageIDs) / sizeof(sentMessageIDs[0]);
+      // for (int i = 0; i < size; i++) {
+        
+      //   // if ack matches previously sent ID
+      //   if strcmp(newMessage.message_ID, sentMessageIDs[i] == 0) {
+          
+      //     // send ack info to iOS
+
+
+      //   }
+
+      // }
+
+      pushMessageData(messageDataQueue_toBT, newMessage);
 
     }
 
     state = STATE_TX;
 }
+
 
 void OnTxDone( void )
 {
