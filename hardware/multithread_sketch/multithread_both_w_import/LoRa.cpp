78eecf644993d546c74e95ed1e6d8bd8f7db801a
brockmiddleton <brockmiddleton4@gmail.com>
2024-04-30 13:12:31 -0600

    Refactor within switch statements - to functions
    Adding code for linked lists to handle acknowledgements

--- hardware/multithread_sketch/multithread_both_w_import/LoRa.cpp
+++ hardware/multithread_sketch/multithread_both_w_import/LoRa.cpp
@@ -22,7 +22,7 @@ static RadioEvents_t RadioEvents;
 #define LORA_IQ_INVERSION_ON                        false
 
 #define RX_TIMEOUT_VALUE                            1000
-#define BUFFER_SIZE                                 30 // Define the payload size here
+#define BUFFER_SIZE                                 50 // Define the payload size here
 
 char txpacket[BUFFER_SIZE];
 char serial[100];
@@ -42,14 +42,20 @@ int16_t sendMessageCount = 0;
 int16_t rssi, rxSize;
 bool lora_idle=true;
 
-// Need a third type of message - ACK's
-// everytime we send a message out, we pop the message from original queue, add to ack_wait_list
-// we only need unique id of message, list of char arrays
-char* sentMessageIDs[100];
 
 const int ident_message = 0x01;
 const int ident_contact = 0x02;
 const int ident_ack = 0x03;
+
+// forward declarations
+void sendContactPing();
+void sendMessageFromiOS(); 
+
+// global instance - contanct ping linked list
+LL* knownContacts = new LL;
+
+// global intance - sent messages linked list
+LL* sentMessageIDs = new LL;
 
 
 // global instance of messageData - contactPing
@@ -132,12 +138,22 @@ void SendReceivecode(void* vpParameters) {
 
   delay(2000); // delay 2s to allow for BLE setup
 
+
+
+  /*                             */
+  /*        INFINITE LOOP        */
+  /*                             */
+
   for (;;) {
     //Serial.println("  Core 1 processing - send/receive");
 
     if (messageDataQueue_toLora.queue.size() >= 1) {
       state=STATE_TX; // new message received from bluetooth
     }
+
+    // else if - 
+    // we have a message that has not been ack'ed
+    // if so, we send one more time, then swith to RECEIVE to listen for ACK
 
 
     switch (state) {
@@ -146,41 +162,21 @@ void SendReceivecode(void* vpParameters) {
       case STATE_TX_CONTACT:
       {
         Serial.println("Building Contact Ping");
-
-        // ping printout
-        pingCount++;
-        Serial.printf("Sending Ping: %d, %d\n", contactPing.message_id, pingCount);
-        sprintf(txpacket, "0;%d;0;0;0", contactPing.message_id);
-
-        // send contact ping
-        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
-        Radio.IrqProcess( );
+  
+        sendContactPing();
       
         delay(3000); // delay 3s 
 
+        state = STATE_RX;
+
         break;
       }
+
       // CASE - message to send from iOS  
       case STATE_TX:
       { // make explicit
 
-        // increment send count
-        sendMessageCount++;
-
-        // pop message packet from waiting queue
-        messageData data_to_send = getMessageData(messageDataQueue_toLora);
-
-
-        // packet prinout
-        Serial.printf("PACKET FROM BT : %d;%s;%s;%d;%s\n", data_to_send.message_type, data_to_send.message_id, data_to_send.message_dest,  data_to_send.size, data_to_send.value);
-        sprintf(txpacket, "%d;%s;%s;%d;%s", data_to_send.message_type, data_to_send.message_id, data_to_send.message_dest,  data_to_send.size, data_to_send.value);
-        Serial.printf(txpacket);
-
-        Serial.println("\nSending...");
-        // send the packet out
-        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); 
-        Radio.IrqProcess( );
-        Serial.println("Sent.");
+        sendMessageFromiOS();
 
         // // add packet ID to ack llst
         // size_t size = sizeof(sentMessageIDs);
@@ -196,7 +192,7 @@ void SendReceivecode(void* vpParameters) {
         Serial.println("into RX mode");
         Radio.Rx(0);             // start channel activity detection     
         Radio.IrqProcess( );
-        delay(5000); // listen for 5s
+        delay(7500); // listen for 7.5s
 
         // Send another contact ping
         state = STATE_TX_CONTACT;
@@ -212,17 +208,8 @@ void SendReceivecode(void* vpParameters) {
 
 }
 
-
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr ) {   
-    memcpy(rxpacket, payload, size ); // copy message 
-    rxpacket[size]='\0';              // terminating char
-    Radio.Sleep( );                   
-
-    // build serial monitor printout
-    char result[100];                             
-    sprintf(result,"\r\nreceived packet \"%s\", length %d\r\n",rxpacket,rxSize);
-    Serial.printf(result);
-
+// From RXDone
+void handleIncomingMessage(messageData newMessage) {
     // THREE OPTIONS ON MESSAGE RECEIVE 
         //  - Contact Ping - append to BT list w contantPing Designation
         //  - Message      - append to BT list w Message Designation
@@ -236,61 +223,135 @@ void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr ) {
 
     */
 
-    messageData newMessage = processMessageString(result);
-    Serial.printf(" New Message from LoRa:\n    Type:%d\n   ID:%d\n   Content:%d", newMessage.message_type, newMessage.message_id, newMessage.value);
+  switch (newMessage.message_type) {
 
-
-    // Determine Packet type
-
-    // contact ping
-    if (newMessage.message_type == 0) {
+    // CONTACT PING
+    case 0:
+    {
 
       // Send contact device MAC ADDR to iOS app, determine if seen before
-      Serial.println("Type is Contant Ping - push to BT Queue");
+      Serial.println("Type is Contant Ping - check if previous contact");
 
-      pushMessageData(messageDataQueue_toBT, newMessage);
+      // traverse linked list
+      if (knownContacts->contains(newMessage.message_id)) {
+        Serial.println("Contact has been previously recorded");
+      }
 
-      
+      else {
+        Serial.println("New Contact. Pushing to BT.");
+        // insert into linked list
+        knownContacts->insert(newMessage.message_id);
+        // insert into queue
+        pushMessageData(messageDataQueue_toBT, newMessage);
 
+      }
+
+      break;
     }
 
-    // new message
-    else if (newMessage.message_type == 1) {
+    // NEW MESSAGE
+    case 1:
+    {
 
-      Serial.println("Type is Message - push to BT Queue");
+      Serial.println("Type is Message - push to BT Queue\nBuild and send ACK");
       
       pushMessageData(messageDataQueue_toBT, newMessage);
 
-      // build, send ack
+      // build ack message
       messageData ack = {2, newMessage.message_id, 0, 0, 0};
+      // add to queue to be sent
       pushMessageData(messageDataQueue_toLora, ack);
 
+      break;
     }
 
-    // ack from previous sent message
-    else {
+    // ACKKNOWLEDGEMENT 
+    case 2:
+    {
+
       Serial.println("Type is ACK");
 
-      // // check ack list n
-      // int size = sizeof(sentMessageIDs) / sizeof(sentMessageIDs[0]);
-      // for (int i = 0; i < size; i++) {
+      // iterate thru linked list of previously sent messages
+      // find sent messages corresponding to ACK
+      if (sentMessageIDs->contains(newMessage.message_id)) {
         
-      //   // if ack matches previously sent ID
-      //   if strcmp(newMessage.message_ID, sentMessageIDs[i] == 0) {
-          
-      //     // send ack info to iOS
+        sentMessageIDs->remove(newMessage.message_id);
+      }
 
+      break;
 
-      //   }
-
-      // }
-
-      pushMessageData(messageDataQueue_toBT, newMessage);
-
+    default:
+      break;
     }
+  }
+}
 
+// From Control Switch
+void sendContactPing() {
+  // ping printout
+  pingCount++;
+  Serial.printf("Sending Ping: %d, %d\n", contactPing.message_id, pingCount);
+  sprintf(txpacket, "0;%d;0;0;0", contactPing.message_id);
+
+  // send contact ping
+  Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
+  Radio.IrqProcess( );
+
+}
+
+// From Control Switch
+void sendMessageFromiOS() {
+
+  // increment send count
+  sendMessageCount++;
+
+  // pop message packet from waiting queue
+  messageData data_to_send = getMessageData(messageDataQueue_toLora);
+
+
+  // packet prinout
+  Serial.printf("PACKET FROM BT (to be sent) : %d;%s;%s;%d;%s\n", data_to_send.message_type, data_to_send.message_id, data_to_send.message_dest,  data_to_send.size, data_to_send.value);
+  sprintf(txpacket, "%d;%s;%s;%d;%s", data_to_send.message_type, data_to_send.message_id, data_to_send.message_dest,  data_to_send.size, data_to_send.value);
+  Serial.printf(txpacket);
+  Serial.println("\n...");
+
+  if (txpacket != nullptr) {
+      Radio.Send((uint8_t*)txpacket, strlen(txpacket));
+      Radio.IrqProcess();
+      Serial.println("Sent.");
+
+      // add packet identifier to sent messages
+      Serial.println("Inserting into sent messages");
+      sentMessageIDs->insert(data_to_send.message_id);
+  } else {
+      Serial.println("Error: txpacket is null.");
+  }
+
+}
+
+
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr ) {   
+    memcpy(rxpacket, payload, size ); // copy message 
+    rxpacket[size]='\0';              // terminating char
+    Radio.Sleep( );                   
+
+    // build serial monitor printout
+    char result[100];                             
+    sprintf(result,"\r\nreceived packet \"%s\", length %d\r\n",rxpacket,rxSize);
+    Serial.printf(result);
+
+    // build messageData object
+    messageData newMessage = processMessageString(result);
+    Serial.printf(" New Message from LoRa:\n    Type:%d\n   ID:%d\n   Content:%d\n", newMessage.message_type, newMessage.message_id, newMessage.value);
+
+
+    // Handle message based on type
+    handleIncomingMessage(newMessage);
+
+    // switch to send mode
     state = STATE_TX;
 }
+
 
 
 void OnTxDone( void )
